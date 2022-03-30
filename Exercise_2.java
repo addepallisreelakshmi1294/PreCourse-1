efb2b84f0fb8ea4d33eb0c18e31f3d24d9dc0969
addepallisreelakshmi1294 <102629547+addepallisreelakshmi1294@users.noreply.github.com>
2022-03-30 12:15:17 -0400

    Update Exercise_2.java

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,6 +1,6 @@
 public class StackAsLinkedList { 
   
-    StackNode root; 
+    StackNode root = null; 
   
     static class StackNode { 
         int data; 
@@ -9,6 +9,7 @@ public class StackAsLinkedList {
         StackNode(int data) 
         { 
             //Constructor here 
+	    next = null;
         } 
     } 
     
@@ -16,11 +17,29 @@ public class StackAsLinkedList {
     public boolean isEmpty() 
     { 
         //Write your code here for the condition if stack is empty. 
+	    return root;
     } 
   
     public void push(int data) 
     { 
         //Write code to push data to the stack. 
+	 Node temp = new Node();
+ 
+        // check if stack (heap) is full. Then inserting an
+        //  element would lead to stack overflow
+        if (temp == null) {
+            System.out.print("\nHeap Overflow");
+            return;
+        }
+ 
+        // initialize data into temp data field
+        temp.data = x;
+ 
+        // put top reference into temp link
+        temp.next = top;
+ 
+        // update top reference
+        top = temp;
     } 
   
     public int pop() 
@@ -28,11 +47,27 @@ public class StackAsLinkedList {
 	//If Stack Empty Return 0 and print "Stack Underflow"
         //Write code to pop the topmost element of stack.
 	//Also return the popped element 
+	    
+	 // check for stack underflow
+        if (top == null) {
+            System.out.print("\nStack Underflow");
+            return;
+        }
+ 
+        // update the top pointer to point to the next node
+        top = (top).next;
     } 
   
     public int peek() 
     { 
         //Write code to just return the topmost element without removing it.
+	     // check for empty stack
+        if (!isEmpty()) {
+            return top.data;
+        }
+        else {
+            System.out.println("Stack is empty");
+            return -1;
     } 
   
 	//Driver code
